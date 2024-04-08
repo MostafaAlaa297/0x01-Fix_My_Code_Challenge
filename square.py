240d7aca1baa2e9b42fdeb3dd043a2df35cd1982
MostafaAlaa297 <elaskary609@gmail.com>
2024-04-08 15:03:36 -0700

    fix square

--- square.py
+++ square.py
@@ -1,28 +1,27 @@
 #!/usr/bin/python3
 
-class square():
+class Square:
     
-    width = 0
-    height = 0
+    side_length = 0
 
-    
+
     def __init__(self, *args, **kwargs):
         for key, value in kwargs.items():
             setattr(self, key, value)
 
     def area_of_my_square(self):
         """ Area of the square """
-        return self.width * self.width
+        return self.side_length * self.side_length
 
     def PermiterOfMySquare(self):
-        return (self.width * 2) + (self.height * 2)
+        return 4 * side_length
 
     def __str__(self):
-        return "{}/{}".format(self.width, self.height)
+        return "{}/{}".format(self.side_length, self.side_length)
 
 if __name__ == "__main__":
 
-    s = square(width=12, height=12)
+    s = Square(self.side_length=12)
     print(s)
     print(s.area_of_my_square())
     print(s.PermiterOfMySquare())
